70dfe10d31d2b3363757c905ab6d9e31d5aedc01
James Smith <james.smith@djalbat.com>
2017-09-12 19:48:26 +0100

    Removed the WebGL demo.

--- es6/examples.js
+++ es6/examples.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = {
-  webGLDemo: require('./examples/webGLDemo'),
   intermediate: require('./examples/intermediate'),
   containerHouse: require('./examples/containerHouse'),
   schoonerCecilie: require('./examples/schoonerCecilie')
