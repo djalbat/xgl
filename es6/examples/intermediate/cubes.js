44c58614f7f2b779ba928d40e07a7f65e8c35928
James Smith <james.smith@djalbat.com>
2017-10-01 19:00:21 +0100

    Simplified the cubes.

--- es6/examples/intermediate/cubes.js
+++ es6/examples/intermediate/cubes.js
@@ -1,59 +1,27 @@
 'use strict';
 
-const necessary = require('necessary');
-
 const React = require('../../react'),
       ColourCube = require('./cube/colour'),
       TextureCube = require('./cube/texture'),
       imageUtilities = require('../../utilities/image');
 
-const { asynchronousUtilities } = necessary,
-      { sequence } = asynchronousUtilities,
-      { preloadImage } = imageUtilities;
+const { preloadImage } = imageUtilities;
 
 function create(colourShader, textureShader, canvas, done) {
-  const source = [
-    'imageMap'
-  ];
+  const source = 'imageMap';
 
   preloadImage(source, function(image) {
-    const callbacks = [
-            createColourCubeCallback,
-            createTextureCubeCallback
-          ],
-          context = {
-            colourShader: colourShader,
-            textureShader: textureShader
-          };
+    const colourCube = <ColourCube offsetPosition={[0, 0, 0]} />,
+          textureCube = <TextureCube offsetPosition={[+2, +2, +2]} />;
 
+    colourCube.createElement(colourShader, textureShader);
+    textureCube.createElement(colourShader, textureShader);
     textureShader.createTexture(image, canvas);
 
-    sequence(callbacks, done, context);
+    done();
   });
 }
 
 module.exports = {
   create: create
 };
-
-function createColourCubeCallback(next, done, context) {
-  const { colourShader, textureShader } = context;
-
-  const offsetPosition = [0, 0, 0],
-        colourCube = <ColourCube offsetPosition={offsetPosition} />;
-
-  colourCube.createElement(colourShader, textureShader);
-
-  next();
-}
-
-function createTextureCubeCallback(next, done, context) {
-  const { colourShader, textureShader } = context;
-
-  const offsetPosition = [+2, +2, +2],
-        textureCube = <TextureCube offsetPosition={offsetPosition} />;
-
-  textureCube.createElement(colourShader, textureShader);
-
-  next();
-}
