a94069e24eafb3cf3230a09497057202d4693b0a
James Smith <james.smith@djalbat.com>
2017-10-22 12:11:19 +0100

    Tidied up prior to simplifying the vertex positions.

--- es6/examples/shapes.js
+++ es6/examples/shapes.js
@@ -5,8 +5,6 @@ require('../jiggle');
 const Canvas = require('../canvas'),
       Scene = require('../element/scene'),
       Camera = require('../element/camera'),
-      Mask = require('../element/canvas/mask'),
-      CompoundedShapes = require('./shapes/compounded'),
       TexturedPlane = require('./common/textured/plane'),
       TexturedCuboid = require('./common/textured/cuboid'),
       ColouredCylinder = require('./common/coloured/cylinder'),
@@ -24,11 +22,9 @@ const shapes = () => {
     <Scene imageMap={imageMap} canvas={canvas}>
       <Camera initialDistance={10} initialOffset={[ 0, 0, 0 ]} canvas={canvas} />
       <TexturedCuboid width={1} height={1} depth={1} position={[ 0, 2, 0 ]} imageName="bricks.jpg" />
-      <Mask />
       <TexturedPlane width={1} height={1} position={[ -1, -1, -1 ]} imageName="gravel.jpg" />
       <ColouredCylinder width={1} height={1} depth={1} position={[ 0, -1, 1 ]} rotations={[ 0, 0, 0 ]} colour={[ 1, 0, 0, 1 ]} />
       <TexturedCylinder width={1} height={1} depth={1} position={[ 0, 1, -1 ]} rotations={[ 0, 90, 90 ]} imageName="grass.jpg" />
-      <CompoundedShapes width={0.1} height={0.1} depth={0.1} rotations={[ 45, 45, 45 ]} position={[ 0, 0, 10 ]} />
     </Scene>
 
   );
