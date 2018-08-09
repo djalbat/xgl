dbb5e94a7815bb41c33eb01424e74fdabfcbe835
James Smith <james.smith@djalbat.com>
2018-08-09 12:23:35 +0100

    Fixed the texture bleeding, apparently.

--- es6/canvas/mixin/texture.js
+++ es6/canvas/mixin/texture.js
@@ -1,19 +1,19 @@
 'use strict';
 
 function createTexture(image) {
-  const { RGBA, LINEAR, UNSIGNED_BYTE, TEXTURE_2D, TEXTURE_MIN_FILTER } = this.context,
-        target = TEXTURE_2D,
-        level = 0,
-        internalFormat = RGBA,
-        format = RGBA,
-        type = UNSIGNED_BYTE,
+	const { RGBA, LINEAR, UNSIGNED_BYTE, TEXTURE_2D, TEXTURE_MIN_FILTER } = this.context,
+				target = TEXTURE_2D,
+				level = 0,
+				internalFormat = RGBA,
+				format = RGBA,
+				type = UNSIGNED_BYTE,
 				pname = TEXTURE_MIN_FILTER,
 				param = LINEAR,
-        texture = this.context.createTexture();
+				texture = this.context.createTexture();
 
-  this.context.bindTexture(target, texture);
+	this.context.bindTexture(target, texture);
 
-  this.context.texImage2D(target, level, internalFormat, format, type, image);
+	this.context.texImage2D(target, level, internalFormat, format, type, image);
 
 	this.context.texParameteri(target, pname, param);
 }
