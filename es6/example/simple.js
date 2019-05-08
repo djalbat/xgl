f9dd6d0d27e650a7778c8cd57a27eb08647ba082
James Smith <james.smith@djalbat.com>
2019-05-08 11:48:59 +0100

    Gotten the simple example working.

--- es6/example/simple.js
+++ es6/example/simple.js
@@ -2,13 +2,18 @@
 
 const jiggle = require('../../index');
 
-const { Canvas, Scene, Camera } = jiggle;
+const ColouredTriangle = require('../element/common/coloured/triangle');
+
+const { Canvas, Scene, Part, Camera } = jiggle;
 
 const canvas = new Canvas();
 
 const simpleExample = () =>
 
   <Scene canvas={canvas}>
+    <Part canvas={canvas}>
+      <ColouredTriangle />
+    </Part>
     <Camera canvas={canvas} initialDistance={5} initialOffset={[ 0, 0, 0 ]} />
   </Scene>
 
