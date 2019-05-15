76116ccc9e770e603db0d6f0ad87019b4be06b0e
James Smith <james.smith@djalbat.com>
2019-05-15 17:58:56 +0100

    Finished the pyramid example.

--- es6/example/element/pyramid.js
+++ es6/example/element/pyramid.js
@@ -4,7 +4,10 @@ const Side = require('./side');
 
 const Pyramid = (properties) => [
 
+  <Side />,
+  <Side rotations={[ 0,  90, 0 ]} />,
   <Side rotations={[ 0, 180, 0 ]} />,
+  <Side rotations={[ 0, 270, 0 ]} />,
 
 ];
 
--- es6/example/pyramid.js
+++ es6/example/pyramid.js
@@ -3,20 +3,11 @@
 const jiggle = require('../../index');
 
 const Pyramid = require('./element/pyramid'),
-      configuration = require('../miscellaneous/configuration'),
-      ColouredSquare = require('./element/colouredSquare');
+      configuration = require('../miscellaneous/configuration');
 
-const { Canvas, Scene, Mask, Part, Camera } = jiggle;
+const { Canvas, Scene, Part, Camera } = jiggle;
 
 const canvas = new Canvas();
-
-const mask =
-
-  <Mask>
-    <ColouredSquare size={[ 0.25, 0.25, 1 ]} position={[ -0.125, +0.125, 1 ]} />
-  </Mask>
-
-;
 
 const pyramidExample = () => {
   preloadImageMap((imageMap) => {
@@ -26,8 +17,7 @@ const pyramidExample = () => {
 
       <Scene canvas={canvas}>
         <Part imageMap={imageMap} imageMapJSON={imageMapJSON}>
-          <ColouredSquare size={[ 0.25, 0.25, 1 ]} position={[ -0.125, +0.125, 1 ]} />
-          <Pyramid mask={mask} />
+          <Pyramid />
         </Part>
         <Camera />
       </Scene>
@@ -52,9 +42,3 @@ function preloadImageMap(callback) {
     callback(imageMap);
   }
 }
-
-/*
-
-    <ColouredSquare size={[ 0.25, 0.25, 1]} position={[ -0.25, +0.25, 0 ]} />
-
- */
