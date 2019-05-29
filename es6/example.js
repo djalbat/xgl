297712dc84e7fe82c07cbb5b276ce75b16610635
James Smith <james.smith@djalbat.com>
2019-05-29 20:56:15 +0100

    Removed the temporary example.

--- es6/example.js
+++ es6/example.js
@@ -3,16 +3,11 @@
 const cubeExample = require('./example/cube'),
       simpleExample = require('./example/simple'),
       maskingExample = require('./example/masking'),
-      pyramidExample = require('./example/pyramid'),
-      temporaryExample = require('./example/temporary');
+      pyramidExample = require('./example/pyramid');
 
 const example = window.location.search.substring(1);  ///
 
 switch (example) {
-  case 'temporary':
-    temporaryExample();
-    break;
-
   case 'simple':
     simpleExample();
     break;
