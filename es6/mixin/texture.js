66b70a57cf5e38bad4519d8275b5c266df0a749d
James Smith <james.smith@djalbat.com>
2019-06-01 10:21:41 +0100

    Improved the texture mapping a little.

--- es6/mixin/texture.js
+++ es6/mixin/texture.js
@@ -1,21 +1,21 @@
 'use strict';
 
 function createTexture(image) {
-	const { RGBA, LINEAR, UNSIGNED_BYTE, TEXTURE_2D, TEXTURE_MIN_FILTER } = this.context,
+	const { RGBA, LINEAR, UNSIGNED_BYTE, TEXTURE_2D, TEXTURE_WRAP_S, TEXTURE_WRAP_T, CLAMP_TO_EDGE, TEXTURE_MIN_FILTER } = this.context,
 				target = TEXTURE_2D,
 				level = 0,
 				internalFormat = RGBA,
 				format = RGBA,
 				type = UNSIGNED_BYTE,
-				pname = TEXTURE_MIN_FILTER,
-				param = LINEAR,
 				texture = this.context.createTexture();
 
 	this.context.bindTexture(target, texture);
 
   this.context.texImage2D(target, level, internalFormat, format, type, image);
 
-	this.context.texParameteri(target, pname, param);
+  this.context.texParameteri(target, TEXTURE_WRAP_S, CLAMP_TO_EDGE);
+  this.context.texParameteri(target, TEXTURE_WRAP_T, CLAMP_TO_EDGE);
+	this.context.texParameteri(target, TEXTURE_MIN_FILTER, LINEAR);
 }
 
 function activateTexture(target) { this.context.activeTexture(target); }
