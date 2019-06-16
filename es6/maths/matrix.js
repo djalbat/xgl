15ea70211186d3a6931cedcf452d282d48a42616
James Smith <james.smith@djalbat.com>
2019-06-16 21:02:10 +0100

    Further cosmetic changes to the maths functions.

--- es6/maths/matrix.js
+++ es6/maths/matrix.js
@@ -49,55 +49,55 @@ function invert2(matrix) {
 }
 
 function invert3(matrix) {
-  const a00 = matrix[0], a01 = matrix[1], a02 = matrix[2],
-        a10 = matrix[3], a11 = matrix[4], a12 = matrix[5],
-        a20 = matrix[6], a21 = matrix[7], a22 = matrix[8],
+  const m00 = matrix[0], m01 = matrix[1], m02 = matrix[2],
+        m10 = matrix[3], m11 = matrix[4], m12 = matrix[5],
+        m20 = matrix[6], m21 = matrix[7], m22 = matrix[8],
 
-        b01 =  a22 * a11 - a12 * a21,
-        b11 = -a22 * a10 + a12 * a20,
-        b21 =  a21 * a10 - a11 * a20,
+        b01 =  m22 * m11 - m12 * m21,
+        b11 = -m22 * m10 + m12 * m20,
+        b21 =  m21 * m10 - m11 * m20,
 
-        determinant = a00 * b01 + a01 * b11 + a02 * b21;
+        determinant = m00 * b01 + m01 * b11 + m02 * b21;
 
   if (determinant !== 0) {
     return ([
 
-      b01 / determinant, (-a22 * a01 + a02 * a21) / determinant, ( a12 * a01 - a02 * a11) / determinant,
-      b11 / determinant, ( a22 * a00 - a02 * a20) / determinant, (-a12 * a00 + a02 * a10) / determinant,
-      b21 / determinant, (-a21 * a00 + a01 * a20) / determinant, ( a11 * a00 - a01 * a10) / determinant,
+      b01 / determinant, (-m22 * m01 + m02 * m21) / determinant, ( m12 * m01 - m02 * m11) / determinant,
+      b11 / determinant, ( m22 * m00 - m02 * m20) / determinant, (-m12 * m00 + m02 * m10) / determinant,
+      b21 / determinant, (-m21 * m00 + m01 * m20) / determinant, ( m11 * m00 - m01 * m10) / determinant,
 
     ]);
   }
 }
 
 function invert4(matrix) {
-  const a00 = matrix[ 0], a01 = matrix[ 1], a02 = matrix[ 2], a03 = matrix[ 3],
-        a10 = matrix[ 4], a11 = matrix[ 5], a12 = matrix[ 6], a13 = matrix[ 7],
-        a20 = matrix[ 8], a21 = matrix[ 9], a22 = matrix[10], a23 = matrix[11],
-        a30 = matrix[12], a31 = matrix[13], a32 = matrix[14], a33 = matrix[15],
+  const m00 = matrix[ 0], m01 = matrix[ 1], m02 = matrix[ 2], m03 = matrix[ 3],
+        m10 = matrix[ 4], m11 = matrix[ 5], m12 = matrix[ 6], m13 = matrix[ 7],
+        m20 = matrix[ 8], m21 = matrix[ 9], m22 = matrix[10], m23 = matrix[11],
+        m30 = matrix[12], m31 = matrix[13], m32 = matrix[14], m33 = matrix[15],
 
-        b00 = a00 * a11 - a01 * a10,
-        b01 = a00 * a12 - a02 * a10,
-        b02 = a00 * a13 - a03 * a10,
-        b03 = a01 * a12 - a02 * a11,
-        b04 = a01 * a13 - a03 * a11,
-        b05 = a02 * a13 - a03 * a12,
-        b06 = a20 * a31 - a21 * a30,
-        b07 = a20 * a32 - a22 * a30,
-        b08 = a20 * a33 - a23 * a30,
-        b09 = a21 * a32 - a22 * a31,
-        b10 = a21 * a33 - a23 * a31,
-        b11 = a22 * a33 - a23 * a32,
+        b00 = m00 * m11 - m01 * m10,
+        b01 = m00 * m12 - m02 * m10,
+        b02 = m00 * m13 - m03 * m10,
+        b03 = m01 * m12 - m02 * m11,
+        b04 = m01 * m13 - m03 * m11,
+        b05 = m02 * m13 - m03 * m12,
+        b06 = m20 * m31 - m21 * m30,
+        b07 = m20 * m32 - m22 * m30,
+        b08 = m20 * m33 - m23 * m30,
+        b09 = m21 * m32 - m22 * m31,
+        b10 = m21 * m33 - m23 * m31,
+        b11 = m22 * m33 - m23 * m32,
 
         determinant = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
 
   if (determinant !== 0) {
     return ([
 
-      (a11 * b11 - a12 * b10 + a13 * b09) / determinant, (a02 * b10 - a01 * b11 - a03 * b09) / determinant, (a31 * b05 - a32 * b04 + a33 * b03) / determinant, (a22 * b04 - a21 * b05 - a23 * b03) / determinant,
-      (a12 * b08 - a10 * b11 - a13 * b07) / determinant, (a00 * b11 - a02 * b08 + a03 * b07) / determinant, (a32 * b02 - a30 * b05 - a33 * b01) / determinant, (a20 * b05 - a22 * b02 + a23 * b01) / determinant,
-      (a10 * b10 - a11 * b08 + a13 * b06) / determinant, (a01 * b08 - a00 * b10 - a03 * b06) / determinant, (a30 * b04 - a31 * b02 + a33 * b00) / determinant, (a21 * b02 - a20 * b04 - a23 * b00) / determinant,
-      (a11 * b07 - a10 * b09 - a12 * b06) / determinant, (a00 * b09 - a01 * b07 + a02 * b06) / determinant, (a31 * b01 - a30 * b03 - a32 * b00) / determinant, (a20 * b03 - a21 * b01 + a22 * b00) / determinant,
+      (m11 * b11 - m12 * b10 + m13 * b09) / determinant, (m02 * b10 - m01 * b11 - m03 * b09) / determinant, (m31 * b05 - m32 * b04 + m33 * b03) / determinant, (m22 * b04 - m21 * b05 - m23 * b03) / determinant,
+      (m12 * b08 - m10 * b11 - m13 * b07) / determinant, (m00 * b11 - m02 * b08 + m03 * b07) / determinant, (m32 * b02 - m30 * b05 - m33 * b01) / determinant, (m20 * b05 - m22 * b02 + m23 * b01) / determinant,
+      (m10 * b10 - m11 * b08 + m13 * b06) / determinant, (m01 * b08 - m00 * b10 - m03 * b06) / determinant, (m30 * b04 - m31 * b02 + m33 * b00) / determinant, (m21 * b02 - m20 * b04 - m23 * b00) / determinant,
+      (m11 * b07 - m10 * b09 - m12 * b06) / determinant, (m00 * b09 - m01 * b07 + m02 * b06) / determinant, (m31 * b01 - m30 * b03 - m32 * b00) / determinant, (m20 * b03 - m21 * b01 + m22 * b00) / determinant,
 
     ]);
   }
@@ -163,18 +163,18 @@ function rotate4(matrix, angle, vector) {
     const s = Math.sin(angle),
           c = Math.cos(angle),
           t = 1 - c,
-          a00 = matrix[ 0],
-          a01 = matrix[ 1],
-          a02 = matrix[ 2],
-          a03 = matrix[ 3],
-          a10 = matrix[ 4],
-          a11 = matrix[ 5],
-          a12 = matrix[ 6],
-          a13 = matrix[ 7],
-          a20 = matrix[ 8],
-          a21 = matrix[ 9],
-          a22 = matrix[10],
-          a23 = matrix[11],
+          m00 = matrix[ 0],
+          m01 = matrix[ 1],
+          m02 = matrix[ 2],
+          m03 = matrix[ 3],
+          m10 = matrix[ 4],
+          m11 = matrix[ 5],
+          m12 = matrix[ 6],
+          m13 = matrix[ 7],
+          m20 = matrix[ 8],
+          m21 = matrix[ 9],
+          m22 = matrix[10],
+          m23 = matrix[11],
           b00 = x * x * t + c,
           b01 = y * x * t + z * s,
           b02 = z * x * t - y * s,
@@ -187,9 +187,9 @@ function rotate4(matrix, angle, vector) {
 
     return ([
 
-      a00 * b00 + a10 * b01 + a20 * b02, a01 * b00 + a11 * b01 + a21 * b02, a02 * b00 + a12 * b01 + a22 * b02, a03 * b00 + a13 * b01 + a23 * b02,
-      a00 * b10 + a10 * b11 + a20 * b12, a01 * b10 + a11 * b11 + a21 * b12, a02 * b10 + a12 * b11 + a22 * b12, a03 * b10 + a13 * b11 + a23 * b12,
-      a00 * b20 + a10 * b21 + a20 * b22, a01 * b20 + a11 * b21 + a21 * b22, a02 * b20 + a12 * b21 + a22 * b22, a03 * b20 + a13 * b21 + a23 * b22,
+      m00 * b00 + m10 * b01 + m20 * b02, m01 * b00 + m11 * b01 + m21 * b02, m02 * b00 + m12 * b01 + m22 * b02, m03 * b00 + m13 * b01 + m23 * b02,
+      m00 * b10 + m10 * b11 + m20 * b12, m01 * b10 + m11 * b11 + m21 * b12, m02 * b10 + m12 * b11 + m22 * b12, m03 * b10 + m13 * b11 + m23 * b12,
+      m00 * b20 + m10 * b21 + m20 * b22, m01 * b20 + m11 * b21 + m21 * b22, m02 * b20 + m12 * b21 + m22 * b22, m03 * b20 + m13 * b21 + m23 * b22,
                              matrix[12],                        matrix[13],                        matrix[14],                        matrix[15],
 
     ]);
@@ -200,25 +200,25 @@ function translate4(matrix, vector) {
   let x = vector[0],
       y = vector[1],
       z = vector[2],
-      a00 = matrix[0],
-      a01 = matrix[1],
-      a02 = matrix[2],
-      a03 = matrix[3],
-      a10 = matrix[4],
-      a11 = matrix[5],
-      a12 = matrix[6],
-      a13 = matrix[7],
-      a20 = matrix[8],
-      a21 = matrix[9],
-      a22 = matrix[10],
-      a23 = matrix[11];
+      m00 = matrix[0],
+      m01 = matrix[1],
+      m02 = matrix[2],
+      m03 = matrix[3],
+      m10 = matrix[4],
+      m11 = matrix[5],
+      m12 = matrix[6],
+      m13 = matrix[7],
+      m20 = matrix[8],
+      m21 = matrix[9],
+      m22 = matrix[10],
+      m23 = matrix[11];
 
   return ([
 
-                                         a00,                                      a01,                                      a02,                                      a03,
-                                         a10,                                      a11,                                      a12,                                      a13,
-                                         a20,                                      a21,                                      a22,                                      a23,
-    a00 * x + a10 * y + a20 * z + matrix[12], a01 * x + a11 * y + a21 * z + matrix[13], a02 * x + a12 * y + a22 * z + matrix[14], a03 * x + a13 * y + a23 * z + matrix[15],
+                                         m00,                                      m01,                                      m02,                                      m03,
+                                         m10,                                      m11,                                      m12,                                      m13,
+                                         m20,                                      m21,                                      m22,                                      m23,
+    m00 * x + m10 * y + m20 * z + matrix[12], m01 * x + m11 * y + m21 * z + matrix[13], m02 * x + m12 * y + m22 * z + matrix[14], m03 * x + m13 * y + m23 * z + matrix[15],
 
   ]);
 }
