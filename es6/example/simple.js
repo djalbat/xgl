2c2fa735de9962db3b8f6f689eb3af7781d6280c
James Smith <james.smith@djalbat.com>
2019-06-22 13:51:57 +0100

    Put the simple example back.

--- es6/example/simple.js
+++ es6/example/simple.js
@@ -13,10 +13,8 @@ const simpleExample = () =>
   <Scene canvas={canvas}>
     <Part>
       <ColouredSquare colour={[ 0, 0, 1 ]} />
-      <ColouredSquare colour={[ 0, 1, 0 ]} rotations={[ 90,   0, 0 ]} />
-      <ColouredSquare colour={[ 1, 0, 0 ]} rotations={[  0, -90, 0 ]} />
     </Part>
-    <DesignCamera initialAngles={[ 225, 45 ]} />
+    <DesignCamera />
   </Scene>
 
 ;
